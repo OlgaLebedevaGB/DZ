b4ad7893df903db2630ff64bfdc4911485dceca6
Lebedeva Olga <128788034+OlgaLebedevaGB@users.noreply.github.com>
2023-04-07 12:13:59 +0300

    Инициализация

---
+++ Seminar1/Program.cs
@@ -0,0 +1,53 @@
+﻿// int num1 = 5;
+// double num2 = 2.34;
+
+// string word = "Good morning!";
+// bool check = true;
+
+// int number1 = 5;
+// int number2 = 8;
+
+// Console.WriteLine("My number are " + number1 + " and " + number2 + " and it is good");
+// Console.WriteLine($"My number are {number1} and {number2} and it is good");
+
+// nint = num;
+
+// Console.Write("Input a number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Your number is " + num);
+
+//Task 1. Напишите программу которая на вход принимает два числа и проверяет
+// 
+
+// Console.Write("Input a first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int quad2 = num1 * num2;
+
+// if(num1 == quad2)
+// {
+//     Console.WriteLine("Yes!");
+// }
+// else
+// {
+//     Console.WriteLine("No!");
+// }
+// Task 2. Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+// Console.Write("Введи положительное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int current = -number;
+
+// while(current <= number)
+// {
+//     Console.Write(current + " ");
+//     current ++;// current = current +1;
+// }
+
+
+
+
