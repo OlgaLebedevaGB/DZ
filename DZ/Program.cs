1551508d0b775b16bcb09decc5c8999a7ebbd885
Lebedeva Olga <128788034+OlgaLebedevaGB@users.noreply.github.com>
2023-04-07 21:30:52 +0300

    DZ выполнено

--- DZ/Program.cs
+++ DZ/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Задача 2: Нипиште программу, которая на вход принимает два числа и выдаёт,
 // какое число большее, а какое меньшее.
+
 // Console.Write("Введите первое число: ");
 // int num1 = Convert.ToInt32(Console.ReadLine());
 // Console.Write("Введите второе число: ");
@@ -16,6 +17,7 @@
 
 // Задача 4: Напишите программу, которая принимает на вход три 
 // числа и выдаёт максимальное из этих чисел.
+
 // Console.WriteLine("Введи первое число: ");
 // int num1 = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine("Введи второе число: ");
@@ -38,7 +40,27 @@
 // Задача 6:Напишите программу, которая на вход принимает число и выдаёт, 
 // является ли число чётным (делится ли оно на два без остатка).
 
+// Console.WriteLine("Введите число");
+// double number = Convert.ToDouble(Console.ReadLine());
+// double ostatok = number % 2;
+// if(ostatok == 0)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
 // Задача 8: Напишите программу, которая на вход принимает число (N), 
 // а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
 
+int i = 2;
 
+while(i <= num)
+{
+    Console.Write(i + " ");
+    i = i + 2;
+}
