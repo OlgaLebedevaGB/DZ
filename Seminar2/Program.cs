9077e7070dd94af3e8f0fe4281bd49237c5b6f6b
Lebedeva Olga <128788034+OlgaLebedevaGB@users.noreply.github.com>
2023-04-12 10:34:56 +0300

    seminar2 сохранение 2

---
+++ Seminar2/Program.cs
@@ -0,0 +1,18 @@
+﻿// семинар 2
+
+// Задача1 Напишите программу, 
+// которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа
+
+int CutNumber(int num)
+{
+    int hunderds = num / 100;
+    int units = num % 10;
+
+    int result = hunderds * 10 + units;
+    return result;
+}
+
+int randNumber = new Random().Next(100, 1000);
+int shortNumber = CutNumber(randNumber);
+
+Console.WriteLine($"New version of {randNumber} is {shortNumber}");
