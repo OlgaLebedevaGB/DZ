25fd56178d72da0d8cc56d61f9ff5770432a2268
Lebedeva Olga <128788034+OlgaLebedevaGB@users.noreply.github.com>
2023-04-18 18:11:01 +0300

    seminar 4

---
+++ Seminar3/Program.cs
@@ -0,0 +1,85 @@
+﻿// void ShowRange(int quad)
+// {
+//     if(quard ==1) Console.WriteLine("x>0 and y>0");
+//     else if (quard == 2) Console.WriteLine("x < 0 and y > 0");
+//     else if (quard == 3) Console.WriteLine("x < 0 and y < 0");
+//     else if (quard == 4) Console.WriteLine("x > 0 and y < 0");
+//     else Console.WriteLine("Неверный номер четверти");
+// }
+
+// Console.Write("Введите номер четверти");
+// int 
+
+
+// Task 2(not void). Напишите программу, которая принимает на вход координаты точки (X и Y),
+//         и выдаёт номер четверти плоскости, в которой находится эта точка.
+int ShowQuad(int x, int y)
+{
+    if(x > 0 && y > 0) 
+        return 1;
+    else if(x<0 && y>0)
+        return 2;
+    else if(x<0 && y<0)
+        return 3;
+    else if(x>0 && y<0)
+        return 4;
+    else 
+        return -1;
+}
+Console.WriteLine("Input coordinat X");
+int X = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input coordinat Y");
+int Y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ShowQuad(X,Y));
+
+
+
+// Task 3. Напишите программу, 
+//         которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+int num = EnterNum("Введите число: ");
+Quad(num);
+
+
+int EnterNum(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void Quad(int num)
+{
+    int current = 1;
+    while(current <= num)
+    {
+        Console.WriteLine($"{current} - {current * current}");
+        current++;
+    }
+}
+
+
+// Task 4. Напишите программу, которая принимает на вход координаты двух точек 
+//         и находит расстояние между ними в 2D пространстве.
+double Gipot (double x1, double y1, double x2, double y2)
+{
+    double distX = x2-x1;
+    double distY = y2-y1;
+    double result = Math.Sqrt(distX*distX + distY*distY);
+    return result;
+}
+Console.Write ("Введите значение x1 - ");
+int x1 = Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write ("Введите значение y1 - ");
+int y1 = Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write ("Введите значение x2 - ");
+int x2 = Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write ("Введите значение y2 - ");
+int y2 = Convert.ToInt32(Console.ReadLine()); 
+
+double rez = Math.Round(Gipot (x1,y1,x2,y2),4);
+
+Console.Write ($"Расстояние между точками 1 и 2  составляет  - {rez}");
---
+++ Seminar4/Program.cs
@@ -0,0 +1,56 @@
+﻿// // Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе
+int Quality(int number)
+{
+    int quantity = 0;
+    while (number != 0)
+    {
+        number = number / 10; // number/=10;
+        quantity++;
+    } 
+    return quantity;
+}
+Console.WriteLine("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = Quality(num);
+Console.WriteLine(result);
+
+// // Напишите программу, которая принимает на вход число (А)
+//  и выдаёт сумму чисел от 1 до А.
+// int Num (int a)
+// {
+//     int N = 1;
+//     int sum = 0;
+//     while (N<=a)
+//     {
+//         sum = sum + N;
+//         N ++;
+//     }
+//     return sum;   
+// }
+
+//     Console.WriteLine("Ведите число: ");
+//     int a = Convert.ToInt32(Console.ReadLine());
+//     int result = Num(a);
+//     Console.WriteLine(result);
+
+// Напишите программу, которая принимает на вход число N и 
+// выдаёт произведение чисел от 1 до N.
+
+// int Num (int a)
+// {
+//     int N = 1;
+//     int con = 1;
+//     while (N<=a)
+//     {
+//         con = con * N;
+//         N ++;
+//     }
+//     return con;   
+// }
+
+//     Console.WriteLine("Ведите число: ");
+//     int a = Convert.ToInt32(Console.ReadLine());
+//     int result = Num(a);
+//     Console.WriteLine(result);
+
