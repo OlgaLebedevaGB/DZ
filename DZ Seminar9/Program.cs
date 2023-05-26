ca89538dda28ef572927b19051e4ac1da5ba73f7
Lebedeva Olga <128788034+OlgaLebedevaGB@users.noreply.github.com>
2023-05-26 15:34:57 +0300

    Сохранение DZ Seminar 9

---
+++ DZ Seminar9/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке 
+// от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// 1 вариант
+// string NumbersRec(int a, int b)
+// {
+//     if(a <=b) return NumbersRec(a + 1, b) +  $"{a} ";
+//     else return String.Empty;
+// }
+// int a = 1;
+// Console.Write("Задайте значение N: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(NumbersRec(a, b));
+
+// 2 вариант
+void ShowNums(int num)
+{
+    if (num > 1) ShowNums(num-1);
+    Console.Write(num + " ");
+}
+Console.Write("Задайте значение N: ");
+int b = Convert.ToInt32(Console.ReadLine());
+ShowNums(b);
+
+// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// int SumRec(int m, int n)
+// {
+//     if(n < m) return 0;
+//     else return n + SumRec(m, n-1);
+// }
+
+// Console.Write("Задайте значение M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Задайте значение N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(SumRec(m, n));
+
+// Задача 68: Напишите программу вычисления функции 
+// Аккермана с помощью рекурсии. Даны два неотрицательных 
+// числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// int Akkerman(int m, int n)
+// {
+//   if (m == 0) return n + 1;
+//   else if (n == 0) return Akkerman(m - 1, 1);
+//   else return Akkerman(m - 1, Akkerman(m, n - 1));
+// }
+
+// Console.Write("Задайте значение m: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Задайте значение n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int functionAkkerman = Akkerman(m, n);
+// Console.Write($"Функция Аккермана ({m},{n}) = {functionAkkerman} ");
---
+++ Seminar9/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке 
+// от 1 до N. Выполнить с помощью рекурсии.
+// void ShowNums(int num)
+// {
+//     if (num > 1) ShowNums(num-1);
+//     Console.Write(num + " ");
+// }
+
+// ShowNums(5);
+
+// Напишите программу, которая будет принимать на вход число
+// и возвращать сумму его цифр
+
+// int SumOfDigits(int num)
+// {
+//     if(num != 0) return SumOfDigits(num / 10) + num % 10;
+//     return 0;
+// }
+// Console.WriteLine(SumOfDigits(1234));
+
+// Задайте значение m и n 
+// И напишие программу которая будет выводить все натуральные числа
+// в промежутке от M до N
+
+// 1 способ
+// void ShowNums2(int m, int n)
+// {
+//     if (n > m) 
+//     {
+//         ShowNums2(m, n-1);
+//         Console.Write(n + " ");
+//     }
+    
+//     if (m > n) 
+//     {
+//         Console.Write(m + " ");
+//         ShowNums2 (m-1, n);  
+//     }
+//     if (m==n)
+//     Console.Write(m +" ");  
+// }
+// ShowNums2(5, 1);
+
+// 2 способ
+void ShowNums2(int m, int n)
+{
+
+    Console.Write(m + " ");
+    if (m > n) ShowNums2(m-1, n);
+    if (m < n) ShowNums2(m+1, n);
+}
+ShowNums2(1,5);
+
+// Напишите программу котораяна вход принимает два числа
+// А и В и возводит число А в целую степень В
+// int
+// int MultiPlyNum (int A, int B)
+// {
+//     if (B !=0) return MultiPlyNum(A, B-1)*A;
+//     return 1;
+// }
+// Console.WriteLine(MultiPlyNum(2,4));
